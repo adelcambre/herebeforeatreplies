33ae83b5e8e7b3fdf4f8d531c36561e5d5f7b596
Andy Delcambre <adelcambre@engineyard.com>
2009-04-20 09:45:58 -0700

    refactored out duplication in views

--- before.rb
+++ before.rb
@@ -40,113 +40,62 @@ __END__
 
 @@ layout
 %html
-  = yield
+  %head
+    %title Were you here before @replies?
+    %link{:rel => "stylesheet", :type => "text/css", :href => "/screen.css", :media => :screen}
+  %body
+    = yield
+    %p 
+      Created by
+      %a{:href => "http://twitter.com/adelcambre"} @adelcambre
+      \, source code on 
+      %a{:href => "http://github.com/adelcambre/herebeforeatreplies"} github
+
+    :javascript
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+    :javascript
+      try {
+      var pageTracker = _gat._getTracker("UA-8458289-1");
+      pageTracker._trackPageview();
+      } catch(err) {}
 
 @@ index
-%head
-  %title Were you here before @replies?
-  %link{:rel => "stylesheet", :type => "text/css", :href => "/screen.css", :media => :screen}
-%body
-  %h1
-    %form{:action => "/", :method => "post"}
-      %span was
-      %input{:name => "username"}
-      %span here before @replies?
-    
-  %p 
-    Everyone was here before 
-    %a{:href => "http://twitter.com/oprah"}@oprah
+%h1
+  %form{:action => "/", :method => "post"}
+    %span was
+    %input{:name => "username"}
+    %span here before @replies?
+  
+%p 
+  Everyone was here before 
+  %a{:href => "http://twitter.com/oprah"}@oprah
 
-  %p 
-    The real question is whether you were here before
-    %a{:href => "http://blog.twitter.com/2007/05/are-you-twittering-me.html"} @replies
-    were officially added to twitter.
+%p 
+  The real question is whether you were here before
+  %a{:href => "http://blog.twitter.com/2007/05/are-you-twittering-me.html"} @replies
+  were officially added to twitter.
 
-  %p 
-    Created by
-    %a{:href => "http://twitter.com/adelcambre"} @adelcambre
-    \, source code on 
-    %a{:href => "http://github.com/adelcambre/herebeforeatreplies"} github
-    
-  :javascript
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    
-  :javascript
-    try {
-    var pageTracker = _gat._getTracker("UA-8458289-1");
-    pageTracker._trackPageview();
-    } catch(err) {}
-    
 @@ yes
-%head
-  %title== #{params[:user]} was here before @replies
-  %link{:rel => "stylesheet", :type => "text/css", :href => "/screen.css", :media => :screen}
-%body
-  %h1
-    %a{:href => "http://twitter.com/#{params[:user]}"}== @#{params[:user]}
-    was here before @replies
-  
-  %p== #{params[:user]} joined on #{@created.to_s}
-  
-  %p
-    %a{:href => "http://twitter.com/home?status=%40#{params[:user]}+was+here+before+%40replies+http%3A%2F%2Fherebeforeatreplies.com+%23herebeforeatreplies"} Twitter This!
+%h1
+  %a{:href => "http://twitter.com/#{params[:user]}"}== @#{params[:user]}
+  was here before @replies
 
-  %p 
-    Created by: 
-    %a{:href => "http://twitter.com/adelcambre"} @adelcambre
-    
-  :javascript
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+%p== #{params[:user]} joined on #{@created.to_s}
 
-  :javascript
-    try {
-    var pageTracker = _gat._getTracker("UA-8458289-1");
-    pageTracker._trackPageview();
-    } catch(err) {}
+%p
+  %a{:href => "http://twitter.com/home?status=%40#{params[:user]}+was+here+before+%40replies+http%3A%2F%2Fherebeforeatreplies.com+%23herebeforeatreplies"} Twitter This!
 
 @@ no
-%head
-  %title== #{params[:user]} was not here before @replies
-  %link{:rel => "stylesheet", :type => "text/css", :href => "/screen.css", :media => :screen}
-%body
-  %h1
-    %a{:href => "http://twitter.com/#{params[:user]}"}== @#{params[:user]}
-    was not here before @replies
-  
-  %p== #{params[:user]} joined on #{@created.to_s}
-  
-  %p 
-    Created by: 
-    %a{:href => "http://twitter.com/adelcambre"} @adelcambre
-    
-  :javascript
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+%h1
+  %a{:href => "http://twitter.com/#{params[:user]}"}== @#{params[:user]}
+  was not here before @replies
 
-  :javascript
-    try {
-    var pageTracker = _gat._getTracker("UA-8458289-1");
-    pageTracker._trackPageview();
-    } catch(err) {}
-    
+%p== #{params[:user]} joined on #{@created.to_s}
+  
 @@ not_found
-%head
-  %title== #{params[:user]} was not here before @replies
-  %link{:rel => "stylesheet", :type => "text/css", :href => "/screen.css", :media => :screen}
-%body
-  %h1== @#{params[:user]} doesn't exist on twitter
+%h1== @#{params[:user]} doesn't exist on twitter
 
-  %p
-    %a{:href => "/"} Go Back
-
-  :javascript
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-  :javascript
-    try {
-    var pageTracker = _gat._getTracker("UA-8458289-1");
-    pageTracker._trackPageview();
-    } catch(err) {}
+%p
+  %a{:href => "/"} Go Back
